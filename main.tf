ec280a48c59fc8644a454cb5b8edd7f5cfd77520
joao vitor <contatovitordeoliveira@gmail.com>
2020-06-25 23:02:30 -0300

    Adicionado variaveis tfvars

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
 // Configure the Google Cloud provider
 provider "google" {
  credentials = file("CREDENTIALS_FILE.json")
- project     = "striking-joy-281500"
+ project     = var.project_name
  region      = "us-west1"
 }
 
-resource "google_storage_bucket" "bkt-prd-portfolio-censo-superior-joao-teste" {
-  name          = "bkt-prd-portfolio-censo-superior"
+resource "google_storage_bucket" "bkt-prd-portfolio-censo-superior-terraform-teste" {
+  name          = var.bkt_name
+}
+
+variable "bkt_name" {
+    type = string
+    description = "Nome do bucket, precisa ser unico"    
+}
+
+variable "project_name" {
+    type = string
+    description = "Nome do projeto gerado pelo gcp"    
 }
---
+++ variables.tfvars
@@ -0,0 +1,2 @@
+bkt_name = "bkt-prd-portfolio-censo-superior"
+project_name = "striking-joy-281500"
